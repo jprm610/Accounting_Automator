757c36634e1d90c3d9b304089c503841ecab0df5
jprm610 <jprm610@gmail.com>
2021-08-15 14:58:24 -0500

    2

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import numpy as np
 import datetime
 
 def Main() :
+    year = int(input('Año: '))
+    month = int(input('Mes: '))
+
     parsed_data = [] 
 
     conversation_path = 'chat.txt' 
@@ -34,7 +37,45 @@ def Main() :
 
     chat.set_index('date', inplace=True)
 
-    chat.to_csv('df_chat.csv')
+    dates = []
+    descriptions = []
+    ingresos = []
+    gastos = []
+
+    for i in range(len(chat)) :
+        if chat.index[i].year == year and chat.index[i].month :
+            message = chat['Message'].values[i]
+            category = Ingreso_Gasto(message)
+            if category == 0 : continue
+
+            message = message.replace('*', '')
+            words = message.split(' ')
+
+            sum = 0
+            for word in words :
+                if word.isnumeric() :
+                    sum += int(word)
+
+            dates.append(chat.index[i])
+            descriptions.append(message)
+
+            if category == 1 :
+                gastos.append(sum)
+                ingresos.append(0)
+            elif category == 2 :
+                gastos.append(0)
+                ingresos.append(sum)
+            else :
+                gastos.append(0)
+                ingresos.append(0)
+            
+    accounty_df = pd.DataFrame()
+    accounty_df['Fecha'] = np.array(dates)
+    accounty_df['Descripcion'] = np.array(descriptions)
+    accounty_df['Egresos'] = np.array(gastos)
+    accounty_df['Ingresos'] = np.array(ingresos)
+
+    accounty_df.to_csv(f"Relación Contabilidad {month} {year}.csv")
 
 def Start_With_Date_And_Time(s) :
     pattern = "^\d{1,2}/\d{1,2}/\d{1,2}, \d{1,2}:\d{1,2}\S [AaPp][Mm] -"
@@ -61,7 +102,7 @@ def Find_Author(s) :
 
     return False
 
-def Get_Data_Point(line):   
+def Get_Data_Point(line) :
     split_line = line.split(' - ') 
     date_time = split_line[0]
     message = ' '.join(split_line[1:])
@@ -72,5 +113,15 @@ def Get_Data_Point(line):
     else:
         author = None
     return date_time, author, message
+
+def Ingreso_Gasto(description) :
+    
+    if description[:3] == '***' :    # Anotación
+        return 3
+    elif description[:2] == '**' :   # Ingreso
+        return 2
+    elif description[:1] == '*' :    # Gasto
+        return 1
+    else : return 0
 
 Main()
