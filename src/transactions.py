28fa9563feb14611293417ac9eee0b221ea68351
jprm610 <jprm610@gmail.com>
2024-05-11 12:30:49 -0500

    1.4

--- src/transactions.py
+++ src/transactions.py
@@ -1,11 +1,13 @@
 import pandas as pd
+from src.transaction import Transaction
 
 class Transactions :
     def __init__(self, chat_df: pd.DataFrame, year: int, month: int) -> None:
         self.year = year
         self.month = month
         self.chat_df = chat_df
-        self.transactions = self.filterChat(self.chat_df, year=self.year, month=self.month)
+        self.chat_df = self.filterChat(chat_df=self.chat_df, year=self.year, month=self.month)
+        self.transactions = self.convertToTransactionsFormat(chat_df=self.chat_df)
 
     def getTransactions(self) -> pd.DataFrame:
         return self.transactions
@@ -15,8 +17,33 @@ class Transactions :
         chat_df.index = pd.to_datetime(chat_df.index)
         
         # Only transactions that start with an asterisk * and are in the specified year and month
-        transactions = chat_df[(chat_df['Message'].str.startswith('*')) & 
+        chat_df = chat_df[(chat_df['Message'].str.startswith('*')) & 
                         (chat_df.index.year == year) & 
                         (chat_df.index.month == month)]
 
+        chat_df['Message'] = chat_df['Message'].apply(Transactions.normalize)
+
+        return chat_df
+    
+    def convertToTransactionsFormat(self, chat_df: pd.DataFrame) -> pd.DataFrame:
+        transactions = [Transaction(date, message).getTransaction() for date, message in zip(chat_df.index, chat_df['Message'])]
+        transactions = pd.DataFrame(transactions)
+        transactions.set_index('date', inplace=True)
         return transactions
+    
+    @classmethod
+    def normalize(cls, s:str) :
+        replacements = (
+            ('*', ''),
+            ("Á", "A"),
+            ("É", "E"),
+            ("Í", "I"),
+            ("Ó", "O"),
+            ("Ú", "U"),
+        )
+        s = s.upper()
+        for a, b in replacements:
+            s = s.replace(a, b).replace(a.upper(), b.upper())
+        s = s.replace("<SE EDITO ESTE MENSAJE.>", '')
+
+        return s
